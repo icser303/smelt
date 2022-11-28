31b4c98d8cca45886d9872c7d6a1d6f2e556a118
icser303 <114996006+icser303@users.noreply.github.com>
2022-11-28 18:36:04 +0530

    Add files via upload

---
+++ smelt/README.md
@@ -0,0 +1,3 @@
+smelt mod 
+smelts picks,shovels,swords and axes to ingots .
+more items will be added in near future.
---
+++ smelt/init.lua
@@ -0,0 +1,96 @@
+minetest.register_craft({
+	type = "cooking",
+	output = "default:bronze_ingot 3",
+	recipe = "default:pick_bronze",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:steel_ingot 3",
+	recipe = "default:pick_steel",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:mese_crystal 3",
+	recipe = "default:pick_mese",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:diamond 3",
+	recipe = "default:pick_diamond",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:bronze_ingot",
+	recipe = "default:shovel_bronze",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:steel_ingot",
+	recipe = "default:shovel_steel",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:mese_crystal",
+	recipe = "default:shovel_mese",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:diamond",
+	recipe = "default:shovel_diamond",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:bronze_ingot 3",
+	recipe = "default:axe_bronze",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:steel_ingot 3",
+	recipe = "default:axe_steel",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:mese_crystal 3",
+	recipe = "default:axe_mese",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:diamond 3",
+	recipe = "default:axe_diamond",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:bronze_ingot 2",
+	recipe = "default:sword_bronze",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:steel_ingot 2",
+	recipe = "default:sword_steel",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:mese_crystal 2",
+	recipe = "default:sword_mese",
+	cooktime = 50,
+})
+minetest.register_craft({
+	type = "cooking",
+	output = "default:diamond 2",
+	recipe = "default:sword_diamond",
+	cooktime = 50,
+})
